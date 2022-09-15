89d59b820caa88e6ccdc8511dc414207a93265ea
ER-Mollo <mollorefilwe68@gmail.com>
2022-09-15 15:10:21 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import { storage } from './firebase';
+import * as Sharing from 'expo-sharing';
+import { color } from 'react-native-elements/dist/helpers';
 
 export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+  const [message, setMessage] = React.useState("");
+
+  async function startRecording() {
+    try {
+      const permission = await Audio.requestPermissionsAsync();
+
+      if (permission.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+
+        setRecording(recording);
+      } else {
+        setMessage("Please grant permission to app to access microphone");
+      }
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    console.log(updatedRecordings);
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millies) {
+    const minutes = millies/ 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    console.log(minutesDisplay);
+    return `${minutesDisplay}:${secondsDisplay}`;
+    
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+          <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+        </View>
+      );
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text style={styles.AR}>Audio Recorder</Text>
+      </View>
+      <View>
+        <Text>{message}</Text>
+        <Button
+          title={recording ? 'Stop Recording' : 'Start Recording'}
+          onPress={recording ? stopRecording : startRecording} />
+
+        {getRecordingLines()}
+        
+        
+
+        <StatusBar style="auto" />
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    // flex:1,
+    backgroundColor: '#1c0338',
+    // justifyContent: 'center',
+    width:'100vw',
+    height:'100vh',
+  },
+  content:{
+    backgroundColor:'red',
+    padding:10,
+  },
+  AR:{
+    color: '#fff',
+    fontSize:16
+  },
+  row: {
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fill: {
+    flex: 1,
+    margin: 16,
+    color:'#fff'
+  },
+  button: {
+    margin: 16
+  }
 });
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// import { getAnalytics } from "firebase/analytics,
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA0GuK9bndZGMwaGa7Ks-QkPVVMKm-0Vxs",
+  authDomain: "audio-recorder-ec57f.firebaseapp.com",
+  projectId: "audio-recorder-ec57f",
+  storageBucket: "audio-recorder-ec57f.appspot.com",
+  messagingSenderId: "132093916930",
+  appId: "1:132093916930:web:79de22bfba02589256741b",
+  measurementId: "G-51WS6JTXX5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+export const storage = getStorage(app);
